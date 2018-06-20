d76c91219b583df30d610239bf9ead5838ed7ca4
William <wtpwinberg@gmail.com>
2018-06-20 13:12:27 -0700

    Base app working properly

--- src/draw.py
+++ src/draw.py
@@ -11,7 +11,6 @@ graph_data = Graph()
 graph_data.create_test_data()
 print(graph_data.vertexes)
 
-
 N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
@@ -19,9 +18,9 @@ color_list = []
 for vertex in graph_data.vertexes:
     color_list.append(vertex.color)
 
-debug_pallet = Spectral8
-debug_pallet.append('#ff0000')
-debug_pallet.append('#0000ff')
+# debug_pallet = Spectral8
+# debug_pallet.append('#ff0000')
+# debug_pallet.append('#0000ff')
 
 plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
               tools='', toolbar_location=None)
@@ -32,13 +31,18 @@ graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
 graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
 
+# this is drawing edges from start to end
 graph.edge_renderer.data_source.data = dict(
+    # TODO what is happening here???
+    # why all edges start from first vertex: is a list of some kind that has to do with strating points
     start=[0]*N,
-    end=node_indices)
+    end=node_indices)  # is a list of some kind that has to do with ending points
+# print(start)
+# print(end)
 
 # start of layout code
 # circ = [i*2*math.pi/N for i in node_indices]
-x = [v.pos['x'] for v in graph_data.vertexes]
+x = [v.pos['x'] for v in graph_data.vertexes]  # assembling a list
 y = [v.pos['y'] for v in graph_data.vertexes]
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
