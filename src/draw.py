f4769f90acaa9c434285f5fb1f7d490d18fe6ab5
William <wtpwinberg@gmail.com>
2018-06-21 16:07:39 -0700

    Label and plotting functioning

--- src/draw.py
+++ src/draw.py
@@ -2,10 +2,15 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Ellipse, ColumnDataSource, Range1d, LabelSet, Label
 from bokeh.palettes import Spectral8
 
 from graph import *
+
+# only works as square rn
+WIDTH = 600
+HEIGHT = 600
+CIRCLE_SIZE = 40
 
 graph_data = Graph()
 graph_data.create_test_data()
@@ -22,23 +27,33 @@ for vertex in graph_data.vertexes:
 # debug_pallet.append('#ff0000')
 # debug_pallet.append('#0000ff')
 
-plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
+# TODO: make rectangle, rect?
+plot = figure(title='Graph Layout Demonstration', x_range=(0, WIDTH), y_range=(0, HEIGHT),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
-graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
+graph.node_renderer.glyph = Ellipse(
+    height=CIRCLE_SIZE, width=CIRCLE_SIZE, fill_color='color')
 
-# this is drawing edges from start to end
+start_indices = []
+end_indices = []
+
+for start_index, vertex in enumerate(graph_data.vertexes):
+    for e in vertex.edges:
+        start_indices.append(start_index)
+        end_indices.append(graph_data.vertexes.index(e.destination))
+
 graph.edge_renderer.data_source.data = dict(
-    # TODO what is happening here???
-    # why all edges start from first vertex: is a list of some kind that has to do with starting points
-    start=node_indices,
-    end=list(map(lambda x: x + 1, node_indices)))  # is a list of some kind that has to do with ending points
-print("this: %s" % (graph.edge_renderer.data_source.data))
-print("node_indices: %s" % (node_indices))
+    start=start_indices,
+    end=end_indices)
+
+# TODO: test with this code
+# graph.edge_renderer.data_source.data = dict(
+#     start=node_indices,  # list of vertexes to start
+#     end=list(map(lambda x: x + 1, node_indices)))  # to end
 
 # start of layout code
 # circ = [i*2*math.pi/N for i in node_indices]
@@ -49,6 +64,27 @@ graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
+
+# TODO: we run through this loop three times, wtf
+value = [v.value for v in graph_data.vertexes]
+
+label_source = ColumnDataSource(data=dict(
+    x=x,
+    y=y,
+    v=value
+))
+
+labels = LabelSet(x='x',
+                  y='y',
+                  text='v',
+                  level='overlay',
+                  text_align='center',
+                  text_baseline='middle',
+                  source=label_source,
+                  render_mode='canvas')
+
+# investigate plot.add_layout vs plot.renderers.append
+plot.add_layout(labels)
 
 output_file('graph.html')
 show(plot)
